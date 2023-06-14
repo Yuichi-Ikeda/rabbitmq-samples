8047476952b84ae0f261ed1e2183102023ae193f
Yuichi-Ikeda <yuichii@outlook.com>
2023-06-14 13:41:30 +0900

    Update receive.py

--- receive.py
+++ receive.py
@@ -23,7 +23,7 @@ def callback(ch, method, properties, body):
     
     try:
         container = ContainerClient.from_container_url(task['sas-url'])
-        container.upload_blob(name='task-{:06}'.format(task['task-id']), data='Task results here.')
+        container.upload_blob(name='task-{:06}'.format(task['task-id']), data='Task Starting.')
     except Exception as ex:
         print("Exception: " + ex)
 
